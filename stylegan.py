01f0864a6655dab524207b0ba94e5c431eaa577b
morvanzhou <morvanzhou@tencent.com>
2020-12-15 18:01:08 +0800

    change structure

--- stylegan.py
+++ stylegan.py
@@ -3,123 +3,166 @@ from tensorflow import keras
 import numpy as np
 from tensorflow.keras.layers import *
 from utils import InstanceNormalization
+import tensorflow.keras.initializers as initer
 
 
 class AdaNorm(keras.layers.Layer):
-    def __init__(self, exclude_mean=True, epsilon=1e-5):
+    def __init__(self, exclude_mean=False, epsilon=1e-5):
         super().__init__()
-        self.exclude_mean = exclude_mean
         self.epsilon = epsilon
+        self.exclude_mean = exclude_mean
 
-    def call(self, x, trainable=None):
+    def call(self, x, **kwargs):
         ins_mean, ins_sigma = tf.nn.moments(x, axes=[1, 2], keepdims=True)
         top = x if self.exclude_mean else x - ins_mean
-        x = top * (tf.math.rsqrt(ins_sigma + self.epsilon))
-        return x
+        x_ins = top * (tf.math.rsqrt(ins_sigma + self.epsilon))
+        return x_ins
 
 
 class AdaMod(keras.layers.Layer):
-    def __init__(self, trainable=None):
+    def __init__(self):
         super().__init__()
-        self.trainable = trainable
         self.ys, self.yb = None, None
 
+    def call(self, inputs, **kwargs):
+        x, w = inputs
+        s, b = self.ys(w), self.yb(w)
+        o = s * x + b
+        return o
+
     def build(self, input_shape):
-        x_input_shape, w_input_shape = input_shape
+        x_shape, w_shape = input_shape
         self.ys = keras.Sequential([
-            keras.layers.Dense(x_input_shape[-1], input_shape=w_input_shape[1:], trainable=self.trainable),
+            keras.layers.Dense(x_shape[-1], input_shape=w_shape[1:], name="s",
+                               kernel_initializer=initer.RandomNormal(0, 1),
+                               bias_initializer=initer.Constant(1)
+                               ),   # this kernel and bias is important
             keras.layers.Reshape([1, 1, -1])
         ])
         self.yb = keras.Sequential([
-            keras.layers.Dense(x_input_shape[-1], input_shape=w_input_shape[1:], trainable=self.trainable),
+            keras.layers.Dense(x_shape[-1], input_shape=w_shape[1:], name="b",
+                               kernel_initializer=initer.RandomNormal(0, 1)),
             keras.layers.Reshape([1, 1, -1])
         ])  # [1, 1, c] per feature map
 
-    def call(self, inputs, training=None):
-        x, w = inputs
-        o = self.ys(w, training=training) * x + self.yb(w, training=training)
-        return o
+
+class AddNoise(keras.layers.Layer):
+    def __init__(self):
+        super().__init__()
+        self.s = None
+        self.x_shape = None
+
+    def call(self, inputs, **kwargs):
+        x, noise = inputs
+        noise_ = noise[:, :self.x_shape[1], :self.x_shape[2], :]
+        return self.s * noise_ + x
+
+    def build(self, input_shape):
+        self.x_shape, _ = input_shape
+        self.s = self.add_weight(name="noise_scale", shape=[1, 1, self.x_shape[-1]],
+                                 initializer=initer.RandomNormal(0., .5))   # large initial noise
+
+
+class Map(keras.layers.Layer):
+    def __init__(self, size, num_layers):
+        super().__init__()
+        self.size = size
+        self.num_layers = num_layers
+        self.f = None
+
+    def call(self, inputs, **kwargs):
+        w = self.f(inputs)
+        return w
+
+    def build(self, input_shape):
+        self.f = keras.Sequential([
+            Dense(self.size, input_shape=input_shape[1:]),
+            # keras.layers.LeakyReLU(0.2),  # worse performance when using non-linearity in mapping
+        ]+[Dense(self.size) for _ in range(self.num_layers - 1)]
+        )
+
+
+class Style(keras.layers.Layer):
+    def __init__(self, filters, upsampling=True, exclude_mean=False):
+        super().__init__()
+        self.filters = filters
+        self.upsampling = upsampling
+        self.exclude_mean = exclude_mean
+        self.ada_mod, self.ada_norm, self.add_noise, self.up, self.conv = None, None, None, None, None
+
+    def call(self, inputs, **kwargs):
+        x, w, noise = inputs
+        x = self.ada_mod((x, w))
+        if self.up is not None:
+            x = self.up(x)
+        x = self.conv(x)
+        x = self.ada_norm(x)
+        x = keras.layers.LeakyReLU()(x)
+        x = self.add_noise((x, noise))
+        return x
+
+    def build(self, input_shape):
+        self.ada_mod = AdaMod()
+        self.ada_norm = AdaNorm(exclude_mean=self.exclude_mean)
+        if self.upsampling:
+            self.up = keras.layers.UpSampling2D((2, 2), interpolation="bilinear")
+        self.add_noise = AddNoise()
+        self.conv = keras.layers.Conv2D(self.filters, 3, 1, "same")
+
+
+def get_generator(latent_dim, img_shape, exclude_mean):
+    n_style_block = 0
+    const_size = _size = 8
+    while _size <= img_shape[1]:
+        n_style_block += 1
+        _size *= 2
+
+    z = keras.Input((n_style_block, latent_dim,), name="z")
+    noise_ = keras.Input((img_shape[0], img_shape[1]), name="noise")
+    ones = keras.Input((1,), name="ones")
+
+    const = keras.Sequential([
+        keras.layers.Dense(const_size * const_size * 128, use_bias=False, name="const"),
+        keras.layers.Reshape((const_size, const_size, 128)),
+    ], name="const")(ones)
+
+    w = Map(size=128, num_layers=3)(z)
+    noise = tf.expand_dims(noise_, axis=-1)
+
+    x = AddNoise()((const, noise))
+    x = AdaNorm(exclude_mean=exclude_mean)(x)
+    x = Style(64, upsampling=False)((x, w[:, 0], noise))  # 7^2
+    for i in range(1, n_style_block):
+        x = Style(64)((x, w[:, i], noise))
+    o = keras.layers.Conv2D(img_shape[-1], 5, 1, "same", activation=keras.activations.tanh)(x)
+
+    g = keras.Model([ones, z, noise_], o, name="generator")
+    g.summary()
+    return g, n_style_block
 
 
 class StyleGAN(keras.Model):
     def __init__(self, img_shape, latent_dim, exclude_mean,
-                 summary_writer=None, lr=0.0002, beta1=0.5, beta2=0.99, lambda_=10, ls_loss=False):
+                 summary_writer=None, lr=0.0002, beta1=0.5, beta2=0.99, lambda_=10,):
         super().__init__()
         self.img_shape = img_shape
         self.latent_dim = latent_dim
-        self.ls_loss = ls_loss
         self.exclude_mean = exclude_mean
-        self._b_scale_count = 0
         self.lambda_ = lambda_
 
-        self.const = tf.random.normal([4, 4, 256], 0, 0.05)
-        self.f = self._get_f()
-        self.g = self._get_generator()
+        self.g, self.n_style_block = get_generator(latent_dim, img_shape, exclude_mean)
+        self.g.summary()
         self.d = self._get_discriminator()
 
         self.opt = keras.optimizers.Adam(lr, beta_1=beta1, beta_2=beta2)
-        self.d_loss_fun = keras.losses.MeanSquaredError() if self.ls_loss \
-            else keras.losses.BinaryCrossentropy(from_logits=True, label_smoothing=0.1)
 
         self.summary_writer = summary_writer
         self._train_step = 0
 
     def call(self, inputs, training=None, mask=None):
         if isinstance(inputs[0], np.ndarray):
-            inputs = (tf.convert_to_tensor(i) for i in inputs)
+            inputs = [tf.convert_to_tensor(i) for i in inputs]
         return self.g.call(inputs, training=training)
-
-    @staticmethod
-    def _get_f():
-        f = keras.Sequential([
-            Dense(128),
-            LeakyReLU(0.2),
-            Dense(128),
-            LeakyReLU(0.2),
-            Dense(256),
-            LeakyReLU(0.2),
-        ], name="f")
-        return f
-
-    def _get_generator(self):
-        z1 = keras.Input((self.latent_dim,))
-        z2 = keras.Input((self.latent_dim,))
-        noise_ = keras.Input((self.img_shape[0], self.img_shape[1]))
-        noise = tf.expand_dims(noise_, axis=-1)
-
-        w1 = self.f(z1)
-        w2 = self.f(z2)
-
-        x = self.add_noise(self.const, noise)
-        x = AdaNorm(exclude_mean=self.exclude_mean)(x)
-        x = self.style_block(256, x, w1, noise, upsampling=False)  # 4^2
-        x = self.style_block(128, x, w1, noise)  # 8^2
-        x = self.style_block(128, x, w1, noise)  # 16^2
-        x = self.style_block(128, x, w1, noise)  # 32^2
-        x = self.style_block(256, x, w2, noise)  # 64^2
-        x = self.style_block(128, x, w2, noise)  # 128^2
-        o = Conv2D(3, 7, 1, "same", activation="tanh")(x)
-        g = keras.Model([z1, z2, noise_], o, name="generator")
-        g.summary()
-        return g
-
-    def style_block(self, filters, x, w, b_noise, upsampling=True):
-        x = AdaMod()((x, w))
-        if upsampling:
-            x = keras.layers.UpSampling2D((2, 2), interpolation="bilinear")(x)
-
-        x = keras.layers.Conv2D(filters, 3, 1, "same")(x)
-        x = self.add_noise(x, b_noise)
-        x = keras.layers.ReLU()(x)
-        x = AdaNorm(exclude_mean=self.exclude_mean)(x)
-        return x
-
-    def add_noise(self, x, b_noise):
-        x_shape = x.shape[1:] if x.shape[0] is None else x.shape
-        b_noise_ = b_noise[:, :x_shape[0], :x_shape[1], :]
-        scale = self.add_weight(name="b_scale{}".format(self._b_scale_count), shape=[1, 1, x.shape[-1]])
-        self._b_scale_count += 1
-        return scale * b_noise_ + x
 
     def _get_discriminator(self):
         def add_block(filters, do_norm=True):
@@ -135,10 +178,9 @@ class StyleGAN(keras.Model):
         add_block(64, do_norm=False)   # -> 64^2
         add_block(128)                   # -> 32^2
         add_block(256)                  # -> 16^2
-        add_block(512)                  # -> 8^2
-        # add_block(512)                # -> 4^2
+        add_block(256)                  # -> 8^2
 
-        model.add(Conv2D(128, 3, 2, "valid"))
+        model.add(Conv2D(64, 3, 2, "valid"))
         model.add(InstanceNormalization(exclude_mean=exclude_mean))
         model.add(Flatten())
         model.add(keras.layers.Dense(128))
@@ -164,14 +206,18 @@ class StyleGAN(keras.Model):
         # the distance of two data distributions
         return tf.reduce_mean(real) - tf.reduce_mean(fake)
 
+    def get_inputs(self, n):
+        available_z = [tf.random.normal((n, 1, self.latent_dim)) for _ in range(2)]
+        z = tf.concat([available_z[np.random.randint(0, len(available_z))] for _ in range(self.n_style)], axis=1)
+
+        noise = tf.random.normal((n, self.img_shape[0], self.img_shape[1]))
+        return [tf.ones((n, 1)), z, noise]
+
     def train_d(self, img):
         n = len(img)
-        z1 = tf.random.normal((n, self.latent_dim))
-        z2 = tf.random.normal((n, self.latent_dim)) if np.random.random() < 0.5 else z1
-        noise = tf.random.normal((n, self.img_shape[0], self.img_shape[1]))
-        inputs = (z1, z2, noise)
+
         with tf.GradientTape() as tape:
-            gimg = self.call(inputs, training=False)
+            gimg = self.call(self.get_inputs(n), training=False)
             gp = self.gp(img, gimg)
             pred_fake = self.d.call(gimg, training=True)
             pred_real = self.d.call(img, training=True)
@@ -188,17 +234,12 @@ class StyleGAN(keras.Model):
         return gp, w_distance
 
     def train_g(self, n):
-        z1 = tf.random.normal((n, self.latent_dim))
-        z2 = tf.random.normal((n, self.latent_dim)) if np.random.random() < 0.5 else z1
-        noise = tf.random.normal((n, self.img_shape[0], self.img_shape[1]))
-        inputs = (z1, z2, noise)
         with tf.GradientTape() as tape:
-            gimg = self.call(inputs, training=True)
+            gimg = self.call(self.get_inputs(n), training=True)
             pred_fake = self.d.call(gimg, training=False)
             w_distance = tf.reduce_mean(-pred_fake)  # minimize W distance
-        var = self.g.trainable_variables + self.f.trainable_variables
-        grads = tape.gradient(w_distance, var)
-        self.opt.apply_gradients(zip(grads, var))
+        grads = tape.gradient(w_distance, self.g.trainable_variables)
+        self.opt.apply_gradients(zip(grads, self.g.trainable_variables))
 
         if self._train_step % 300 == 0 and self.summary_writer is not None:
             with self.summary_writer.as_default():
